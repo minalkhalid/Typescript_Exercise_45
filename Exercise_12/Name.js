c8fc45018c2a7133361e239a748be9aa3446d335
minalkhalid <160639621+minalkhalid@users.noreply.github.com>
2024-06-03 18:26:08 +0500

    Add files via upload

---
+++ Exercise_12/Name.js
@@ -0,0 +1,6 @@
+"use strict";
+let names = ['mianl', 'zoya', 'wania', 'hamna'];
+const massage = 'welcome to ai class: ';
+for (let i = 0; i < names.length; i++) {
+    console.log(massage + names[i]);
+}
---
+++ Exercise_12/Name.ts
@@ -0,0 +1,11 @@
+
+
+let names :string[] =['mianl','zoya','wania','hamna'] ;
+
+const massage :string = 'welcome to ai class: '
+
+for(let i=0; i<names.length ; i++){
+   
+    console.log(massage + names[i])
+   
+}
