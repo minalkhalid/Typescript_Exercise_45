3ea26d2995cd4c5af388d7b283f1c6a61ae1916e
minalkhalid <160639621+minalkhalid@users.noreply.github.com>
2024-06-03 18:05:11 +0500

    Add files via upload

---
+++ Exercise_05/Famous_Quote_2.ts
@@ -0,0 +1,5 @@
+let famous_person :string = 'Albert Einstein';
+
+let massage :string =' “A person who never made a mistake never tried anything new.”';
+
+console.log(`${famous_person} once said, ${ massage}`);
