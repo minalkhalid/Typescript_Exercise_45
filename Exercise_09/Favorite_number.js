6ee52bf04885aacc87986ac267bf2087c2ab59ae
minalkhalid <160639621+minalkhalid@users.noreply.github.com>
2024-06-03 18:15:26 +0500

    Add files via upload

---
+++ Exercise_09/Favorite_number.js
@@ -0,0 +1,4 @@
+"use strict";
+const favorute_number = 10;
+const massage = 'My favorite number ' + favorute_number;
+console.log(massage);
---
+++ Exercise_09/Favorite_number.ts
@@ -0,0 +1,5 @@
+const favorute_number : number = 10 ;
+
+const massage :string ='My favorite number '+ favorute_number ;
+
+console.log(massage) ;
