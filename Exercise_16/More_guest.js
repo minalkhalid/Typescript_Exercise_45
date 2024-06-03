4b90dfed405ce58cde777fc8354d5db274bc7944
minalkhalid <160639621+minalkhalid@users.noreply.github.com>
2024-06-03 18:42:35 +0500

    Add files via upload

---
+++ Exercise_16/More_guest.js
@@ -0,0 +1,19 @@
+"use strict";
+let Guest_list = ['zain', 'imran', 'rohan'];
+//for(let i=0;  i<Guest_list.length; i++ ){
+// console.log('Dear Mr.'  + Guest_list[i] + ' ,\n\nit is our pleasure to invite you in our party.\n\nThank you!\n\n')
+//}
+let absent_Guest = 'zain';
+let new_Guest = 'kamran';
+Guest_list[0] = new_Guest;
+for (let i = 0; i < Guest_list.length; i++) {
+    console.log('Dear Mr.' + Guest_list[i] + ',\n\nit is our pleasure to invite you in our party.\n\nThank you!\n\n');
+}
+console.log(`Mr.${absent_Guest} is not coming to the party.`);
+console.log('Good News! we find the Big table so we are inviting 3 more guest');
+Guest_list.unshift('sir hamza khan');
+Guest_list.splice(2, 0, 'zuhaib khan');
+Guest_list.push('khalid mehmood bhatti');
+for (let i = 0; i < Guest_list.length; i++) {
+    console.log('Dear Mr.' + Guest_list[i] + ',\n\nit is our pleasure to invite you in our party.\n\nThank you!\n\n');
+}
