f2d703fb7ed74b488fc4266ba074c8b4cbe24b04
minalkhalid <160639621+minalkhalid@users.noreply.github.com>
2024-06-03 18:22:46 +0500

    Add files via upload

---
+++ Exercise_11/Names.js
@@ -0,0 +1,5 @@
+"use strict";
+let names = ['minal', 'zoya', 'wania', 'hamna'];
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
---
+++ Exercise_11/Names.ts
@@ -0,0 +1,5 @@
+let names:string[] =['minal','zoya','wania','hamna'] ;
+
+for(let i=0; i<names.length ; i++){
+console.log(names[i])
+}
