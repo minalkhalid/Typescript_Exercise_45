303fead3555da0e06d4448490848bdfb3e0431f6
minalkhalid <160639621+minalkhalid@users.noreply.github.com>
2024-06-03 18:28:38 +0500

    Add files via upload

---
+++ Exercise_13/Your_own_array.js
@@ -0,0 +1,5 @@
+"use strict";
+let transportation = ['bike', 'car', 'train', 'bus'];
+for (let i = 0; i < transportation.length; i++) {
+    console.log('i would like to own  a ' + transportation[i]);
+}
---
+++ Exercise_13/Your_own_array.ts
@@ -0,0 +1,9 @@
+
+
+let transportation :string[] =['bike','car','train','bus'] ;
+
+for(let i=0; i<transportation.length ; i++){
+console.log('i would like to own a' + transportation[i])
+
+}
+
