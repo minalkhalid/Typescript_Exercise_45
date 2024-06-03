b471e66215b1fc57a0eb625733f2af48b825f1d0
minalkhalid <160639621+minalkhalid@users.noreply.github.com>
2024-06-03 18:33:40 +0500

    Add files via upload

---
+++ Exercise_14/Guest_list.ts
@@ -0,0 +1,9 @@
+
+
+let Guest_list :string[] =['fayyash','imran','abeer'] ;
+
+for(let i=0;  i<Guest_list.length; i++ ){
+
+    console.log('Dear Mr.'  + Guest_list[i] + ' ,\n\nit is our pleasure to invite you in our party.\n\nThank you!')
+
+}
