c4777d796460b589d9d42364cfaa19d07567ba6b
minalkhalid <160639621+minalkhalid@users.noreply.github.com>
2024-06-05 15:58:56 +0500

    Add files via upload

---
+++ Exercise_20/Empty_Array.js
@@ -0,0 +1,9 @@
+"use strict";
+//Think of someting you could store in array.
+let items = ['pakistan', 'karachi', 'urdu'];
+//write a program that creates a list containing these items.
+let item2 = [];
+item2.push('france');
+item2.push('pakistan');
+item2.push('india');
+console.log(item2);
---
+++ Exercise_20/Empty_Array.ts
@@ -0,0 +1,19 @@
+
+//Think of someting you could store in array.
+let items :string[] = ['pakistan', 'karachi', 'urdu'];
+
+//write a program that creates a list containing these items.
+
+let item2 :string[] = [];
+
+item2.push('france');
+item2.push('pakistan');
+item2.push('india');
+
+console.log(item2);
+
+
+
+
+
+
