d160fb9b0c1fbac9b66ea92eb0dbdfa51ef0a99f
minalkhalid <160639621+minalkhalid@users.noreply.github.com>
2024-06-05 16:00:50 +0500

    Add files via upload

---
+++ Exercise_21/Typescript_object.js
@@ -0,0 +1,16 @@
+"use strict";
+// write a program that creates objects containing these items.
+let person = {
+    age: 26,
+    name: 'minal',
+    nationality: 'pakistan',
+    student: true,
+};
+console.log(person);
+// car object
+let car = {
+    maker: 'honda',
+    color: 'green',
+    automatic: true
+};
+console.log(car);
---
+++ Exercise_21/Typescript_object.ts
@@ -0,0 +1,40 @@
+
+// write a program that creates objects containing these items.
+
+interface person{
+    age : number,
+    name : string ,
+    nationality : string ,
+    student : Boolean ,
+}
+
+
+let person :person = {
+
+    age : 26 ,
+    name : 'minal' ,
+    nationality : 'pakistan' ,
+    student : true ,
+    
+
+}
+
+console.log(person);
+
+interface car  {
+    makar :string,
+    color :string,
+    automatic :boolean
+}
+
+
+
+// car object
+let car = {
+    maker:'honda',
+    color : 'green',
+    automatic : true
+
+
+}
+console.log(car);
