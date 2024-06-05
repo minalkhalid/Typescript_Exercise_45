63f6fca71a225d85dff57bbec5f4dcda6e2c5112
minalkhalid <160639621+minalkhalid@users.noreply.github.com>
2024-06-05 16:05:48 +0500

    Add files via upload

---
+++ Exercise_22/Intentional_Error.ts
@@ -0,0 +1,12 @@
+// If you haven’t received an array index error in one of your programs yet, try to make one happen.
+
+// creating array 
+let newArray :string[] =['baber azam','rizwan khan','naseen shah'];
+
+// Accessing array by index
+console.log(newArray[10]);
+
+// make sure you correct the error before closing the program.
+
+// correct the error 
+console.log(newArray);
