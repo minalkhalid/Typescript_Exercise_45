6501a3fcd13e6081af9c8e88a06e5132bb5c6764
minalkhalid <160639621+minalkhalid@users.noreply.github.com>
2024-06-06 15:49:43 +0500

    Add files via upload

---
+++ Exercise_43/unchanged_magicians.js
@@ -0,0 +1,22 @@
+"use strict";
+//define the function to show magicians names
+function show_magicians(magicians) {
+    magicians.forEach(name => console.log(name));
+}
+// function  to make magicians great through .map() it will modify array
+function make_great(magicians) {
+    return magicians.map(name => `The Great ${name}`);
+}
+//define an array of magicians names
+let magician_names = ["Harry poter", "Hermione Granger", "Ron weasley"];
+// Making a copy of original array through .slice function
+let copy_magician_names = magician_names.slice();
+//modify  the copied array to include "The Great" with their names
+let copy_great_magician = make_great(copy_magician_names);
+// show both arrays original and copied
+// original 
+console.log("\noriginal array\n");
+show_magicians(magician_names);
+//copied
+console.log("\ncopied\n");
+show_magicians(copy_great_magician);
