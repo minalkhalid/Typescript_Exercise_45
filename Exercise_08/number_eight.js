7a453909a8d6745b6b3f571a8710ca37dd89eb14
minalkhalid <160639621+minalkhalid@users.noreply.github.com>
2024-06-28 17:12:58 +0500

    Add files via upload

---
+++ Exercise_08/number_eight.js
@@ -0,0 +1,8 @@
+"use strict";
+//you should create four lines that look like this:
+//console.log(5 + 3)
+//your output should simply be four lines with the number 8 appearing once on each line.
+console.log(5 + 3);
+console.log(11 - 3);
+console.log(8 * 1);
+console.log(64 / 8);
