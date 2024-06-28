4b10cd26c55da6c96e363251707f9dd3bd10044b
minalkhalid <160639621+minalkhalid@users.noreply.github.com>
2024-06-28 17:12:08 +0500

    Add files via upload

---
+++ Exercise_07/Number_Eight.js
@@ -0,0 +1,7 @@
+"use strict";
+/*write addition, subtraction, multiplication, and division operations that each result in the number 8.
+ Be sure to enclose your operations in print statements to see the results.*/
+console.log("Addition:", 4 + 4);
+console.log("Subtraction:", 10 - 2);
+console.log("Mulitiplication:", 2 * 4);
+console.log("Division:", 16 / 2);
